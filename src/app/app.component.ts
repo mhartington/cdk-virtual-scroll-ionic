47a07419b6ec0bf8b0a2e57c50003cc98a2d775d
Mike Hartington <mikehartington@gmail.com>
2019-05-31 12:02:32 -0400

    bump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, SplashScreenPlugin } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  splashScreen: SplashScreenPlugin;
+  constructor() {
+    const { SplashScreen } = Plugins;
+    this.splashScreen = SplashScreen;
+    this.splashScreen.hide();
   }
 }
